d7b6d5bfd5e236d5377459cb2848677877852264
Gustavo Rodrigues <Gustavorr001@gmail.com>
2024-01-22 12:03:23 -0300

    first commit

---
+++ src/main/java/com/techgustavorodrigues/wsspringmongo/WsSpringmongoApplication.java
@@ -0,0 +1,13 @@
+package com.techgustavorodrigues.wsspringmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsSpringmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsSpringmongoApplication.class, args);
+	}
+
+}
