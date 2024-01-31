55c7f4e49e2566d241c217c7232709a61e1dba34
Gustavo Rodrigues <Gustavorr001@gmail.com>
2024-01-31 11:32:07 -0300

    author data projection with dto

--- src/main/java/com/techgustavorodrigues/wsspringmongo/config/Instantiation.java
+++ src/main/java/com/techgustavorodrigues/wsspringmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.techgustavorodrigues.wsspringmongo.domain.Post;
 import com.techgustavorodrigues.wsspringmongo.domain.User;
+import com.techgustavorodrigues.wsspringmongo.dto.AuthorDTO;
 import com.techgustavorodrigues.wsspringmongo.repository.IPostRepository;
 import com.techgustavorodrigues.wsspringmongo.repository.IUserRepository;
 
@@ -38,8 +39,8 @@ public class Instantiation implements CommandLineRunner{
 		
 		iuserRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
-		Post post1 = new Post(null, sdf.parse("29/01/2024"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("30/01/2024"), "Bom dia", "Acordei feliz hoje!", maria);
+		Post post1 = new Post(null, sdf.parse("29/01/2024"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("30/01/2024"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
 		ipostReposiroty.saveAll(Arrays.asList(post1, post2));
 	}
